4ad2f53b453ac26dde58686bfcfbaabaff8fe1e5
Michael Eduave <meduave@zoogtech.com>
2015-10-13 23:40:37 +0800

    make user of dynamic templating add nav buttons partial complete of wizard ui

--- client/scripts/addchild/addchild.js
+++ client/scripts/addchild/addchild.js
@@ -1,26 +1,10 @@
+"use strict";
+
 Template.addchild.created = function () {
   this.currentPage = new ReactiveVar(0);
 };
 
 Template.addchild.rendered = function () {
-
-  var $container = $('.step');
-  var $p = $('p');
-  var currentIndex = 0;
-
-  $p.on('click', function (e) {
-    var $current = $(e.currentTarget);
-    var index = $p.index($current);
-    if (index > currentIndex) {
-      $container.addClass('forward');
-    } else {
-      $container.removeClass('forward');
-    }
-    currentIndex = index;
-    console.log(index);
-    $container.attr('data-step', index);
-  });
-
   this.$("#birthday").datetimepicker({
     format : "MMM-DD-YYYY"
   });
@@ -67,6 +51,23 @@ Template.addchild.rendered = function () {
     placeholder : "Specify who",
     tags : [""]
   });
+
+  var that = this
+
+  this.autorun(function () {
+    var $container = $('.step'),
+      $wizpoint = $('.wiz-point'),
+      currentIndex = that.currentPage.get(),
+      index = $wizpoint.index($wizpoint[currentIndex]);
+
+    if (index > currentIndex) {
+      $container.addClass('forward');
+    } else {
+      $container.removeClass('forward');
+    }
+    $container.attr('data-step', currentIndex);
+  });
+
 };
 
 Template.addchild.destroyed = function () {
@@ -74,17 +75,54 @@ Template.addchild.destroyed = function () {
 };
 
 Template.addchild.events({
-  "click #basicInfoNextBtn" : function (evt, tmpl) {
-
+  "click #nextpage" : function (evt, tmpl) {
+    var curPage = tmpl.currentPage.get();
+    ++curPage;
+    if (curPage > 4) {
+      return;
+    }
+    tmpl.currentPage.set(curPage);
   },
-
-  "click #healthNextBtn" : function (evt, tmpl) {
-    $('.nav-tabs a[href="#family"]').tab('show');
+  "click #prevpage" : function (evt, tmpl) {
+    var curPage = tmpl.currentPage.get();
+    --curPage;
+    if (curPage < 0) {
+      return;
+    }
+    tmpl.currentPage.set(curPage);
   }
 });
 
 Template.addchild.helpers({
   currentPage : function () {
-    return Template.instance().currentPage.get();
+    var page = Template.instance().currentPage.get();
+    switch (page) {
+      case 0:
+        page = "basic";
+      break;
+      case 1:
+        page = "family";
+      break;
+      case 2:
+        page = "health";
+      break;
+      case 3:
+        page = "sponsorship";
+      break;
+      case 4:
+        page = "uploadmedia";
+      break;
+    }
+    return page;
+  },
+
+  showPrevBtn : function () {
+    var page = Template.instance().currentPage.get();
+    return page === 0 ? "hide" : "";
+  },
+
+  nextBtnVal : function () {
+    var page = Template.instance().currentPage.get();
+    return page === 4 ? "Add" : "Next";
   }
-})
+});
